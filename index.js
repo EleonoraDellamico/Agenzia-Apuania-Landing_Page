56fa7750f5e35cfde152040ddad573485247dabb
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-06-01 17:21:42 +0200

    navbar

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function() {
+	links.classList.toggle('show-links');
+});
